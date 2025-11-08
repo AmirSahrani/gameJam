dea4dde0ac905dfd519023f024a5f424cd3c6682
Amir Sahrani <amirsahrani1@gmail.com>
2025-11-08 16:37:16 +0100

    project setup

---
+++ README.md
@@ -0,0 +1,13 @@
+## Game Resources
+Website containing many (free) assets
+https://itch.io/game-assets/free/tag-2d
+
+P5JS reference
+https://p5js.org/reference/
+
+
+
+## Tools
+Ohshitgit is a website to help when you mess up git
+https://ohshitgit.com
+
---
+++ src/index.js
@@ -0,0 +1,31 @@
+let windowWidth = 1000;
+let windowHeight= 800;
+
+let titleXpos = 100;
+let titleYpos = 100;
+let title = "Hello, World!";
+let stepX = 4;
+let stepY = -4;
+
+function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('game-container'); // attach canvas to the div
+}
+
+function draw() {
+    background("#D741A7");
+    textSize(32);
+    stroke("#3A1772");
+    text(title, titleXpos, titleYpos);
+
+    if (titleXpos > windowWidth - textWidth(title) ||
+       (titleXpos < 0)) {
+        stepX *= -1;
+    } 
+    if (titleYpos > windowHeight ||
+       (titleYpos < 32)) {
+        stepY *= -1;
+    }
+    titleXpos += stepX;
+    titleYpos += stepY;
+}
