661e23192b780cace38cdcf3a0a7d915233d342d
Felix Faase <felixfaase2@gmail.com>
2025-11-09 15:48:16 +0100

    moved files

--- src/index.js
+++ src/index.js
@@ -1,55 +1,25 @@
-import * as  player from './player.js'
+import * as  pl from './player.js'
 
 let windowWidth = 1000;
 let windowHeight = 800;
-let player;
 
-let Xspeed, Yspeed;
+let player = new pl.Player(20, windowHeight/3*2);
 
 
 function setup() {
    let canvas = createCanvas(windowWidth, windowHeight);
    canvas.parent('game-container'); // attach canvas to the div
-
-   player = [20, height/3*2, 30, 30]
-   Xspeed = 5; Yspeed = 2;
+   
 }
 
 function draw() {
    background(255);
+   pl.handlePlayerMovement(player);
    line(0, height/3*2+30, width, height/3*2+30);
-   rect(...player);
-
-   if (keyIsDown(RIGHT_ARROW) === true){
-      playerMovesRight();
-   }
-
-   if (keyIsDown(LEFT_ARROW) === true){
-      playerMovesLeft();
-   }
-   keyPressed();
-
-}
-
-function keyPressed(){
-   console.log(keyCode);
   
-   if (keyCode === 38){
-      playerJumps();
-   }
+
 }
 
-function playerMovesLeft(){
-   player[0] = player[0] - Xspeed;
-}
-
-function playerMovesRight(){
-   player[0] = player[0] + Xspeed;
-}
-
-function playerJumps(){
-   player[1] = player[1] + Yspeed^2 - 2*Yspeed;
-}
-// 👇 Make sure p5 can access these:
+// 👇 Make sure p5 can access these:   
 window.setup = setup;
 window.draw = draw;
--- src/player.js
+++ src/player.js
@@ -1,2 +1,59 @@
-export function TEST() {
+let resistance = 0.9;
+
+export class Player {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.dx = 0;
+        this.dy = 0;
+        this.size = [30,30];
+
+    
+    }
+
+    move() {
+        this.dx = this.dx * resistance;
+        if (Math.abs(this.dx) < 1e-6)
+            { this.dx = 0};
+        this.x = this.x + this.dx;
+
+    }
+}
+
+
+export function handlePlayerMovement(player) {
+
+   keyPressed(player);
+
+   player.move();
+   rect(player.x, player.y, ...player.size);
+
+}
+
+   
+
+   
+function keyPressed(player){
+    if (keyIsDown(RIGHT_ARROW) === true){
+      playerMovesRight(player);
+   }
+
+   if (keyIsDown(LEFT_ARROW) === true){
+      playerMovesLeft(player);
+   }
+   if (keyCode === 38){
+      playerJumps(player);
+   }
+}
+
+function playerMovesLeft(p){
+   p.dx = -10;
+}
+
+function playerMovesRight(p){
+   p.dx = 10;
+}
+
+function playerJumps(p){
+   p.dy = 10;
 }
