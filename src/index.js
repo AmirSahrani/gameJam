6a49976bc4f938bdd036030fe9c15be64c4ec3a6
Amir Sahrani <amirsahrani1@gmail.com>
2025-11-09 11:32:49 +0100

    shader dir

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let windowWidth = 1000;
-let windowHeight= 800;
+let windowHeight = 800;
 
 let titleXpos = 100;
 let titleYpos = 100;
@@ -8,24 +8,24 @@ let stepX = 4;
 let stepY = -4;
 
 function setup() {
-    let canvas = createCanvas(windowWidth, windowHeight);
-    canvas.parent('game-container'); // attach canvas to the div
+   let canvas = createCanvas(windowWidth, windowHeight);
+   canvas.parent('game-container'); // attach canvas to the div
 }
 
 function draw() {
-    background("#D741A7");
-    textSize(32);
-    stroke("#3A1772");
-    text(title, titleXpos, titleYpos);
+   background("#D741A7");
+   textSize(32);
+   stroke("#3A1772");
+   text(title, titleXpos, titleYpos);
 
-    if (titleXpos > windowWidth - textWidth(title) ||
-       (titleXpos < 0)) {
-        stepX *= -1;
-    } 
-    if (titleYpos > windowHeight ||
-       (titleYpos < 32)) {
-        stepY *= -1;
-    }
-    titleXpos += stepX;
-    titleYpos += stepY;
+   if (titleXpos > windowWidth - textWidth(title) ||
+      (titleXpos < 0)) {
+      stepX *= -1;
+   }
+   if (titleYpos > windowHeight ||
+      (titleYpos < 32)) {
+      stepY *= -1;
+   }
+   titleXpos += stepX;
+   titleYpos += stepY;
 }
